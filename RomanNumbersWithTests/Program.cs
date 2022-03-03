ff0856715360df0613f448c9d10bbcef605641c6
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2022-03-03 17:35:50 +0700

    Добавьте файлы проекта.

---
+++ RomanNumbersWithTests/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+
+    static void Main()
+    {
+        RomanNumber a = new RomanNumber(2459);
+        RomanNumber b = new RomanNumber(2999);
+        RomanNumber c = new RomanNumber(4);
+        RomanNumber d = new RomanNumber(500);
+        RomanNumber e = new RomanNumber(3999);
+
+        Console.WriteLine("A = 2459 = MMCDLIX: " + a.ToString());
+        Console.WriteLine("B = 2999 = MMCMXCIX: " + b.ToString());
+        Console.WriteLine("C = 4 = IV: " + c.ToString());
+        Console.WriteLine("D = 500 = D : " + d.ToString());
+        Console.WriteLine("");
+
+        Console.WriteLine("B + C = 3003 = MMMIII: " + (b + c).ToString());
+        Console.WriteLine("B - C = 2995 = MMCMXCV: " + (b - c).ToString());
+        Console.WriteLine("D * C = 2000 = MM: " + (d * c).ToString());
+        Console.WriteLine("D / C = 125 = CXXV: " + (d / c).ToString());
+        Console.WriteLine("");
+
+        Console.WriteLine("Сортировка");
+        RomanNumber[] numbers = { a, b, c, d };
+        Array.Sort(numbers);
+        foreach (RomanNumber number in numbers)
+        {
+            Console.WriteLine(number.ToString());
+        }
+        Console.WriteLine("");
+
+        Console.WriteLine("Копирование");
+        var f = (RomanNumber)c.Clone();
+        Console.WriteLine(f.ToString());
+    }
+}
---
+++ RomanNumbersWithTestsTests/RomanNumberTests.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class RomanNumberTests
+    {
+        [TestMethod()]
+        public void RomanNumberTest()
+        {
+            RomanNumber a = new RomanNumber(1);
+            RomanNumber b = new RomanNumber(1234);
+            RomanNumber c = new RomanNumber(3999);
+
+            Assert.ThrowsException<RomanNumberException>(() => new RomanNumber(0));
+            Assert.ThrowsException<RomanNumberException>(() => new RomanNumber(4000));
+            Assert.ThrowsException<RomanNumberException>(() => new RomanNumber(29999));
+        }
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            RomanNumber a = new RomanNumber(1);
+            RomanNumber b = new RomanNumber(123);
+            RomanNumber c = new RomanNumber(456);
+            RomanNumber d = new RomanNumber(789);
+            RomanNumber e = new RomanNumber(1009);
+            RomanNumber f = new RomanNumber(3999);
+
+            Assert.AreEqual("I", a.ToString());
+            Assert.AreEqual("CXXIII", b.ToString());
+            Assert.AreEqual("CDLVI", c.ToString());
+            Assert.AreEqual("DCCLXXXIX", d.ToString());
+            Assert.AreEqual("MIX", e.ToString());
+            Assert.AreEqual("MMMCMXCIX", f.ToString());
+        }
+        [TestMethod()]
+        public void AddTest()
+        {
+            RomanNumber a = new RomanNumber(1);
+            RomanNumber b = new RomanNumber(500);
+            RomanNumber c = new RomanNumber(1000);
+            RomanNumber d = new RomanNumber(3999);
+            RomanNumber n = null;
+
+            RomanNumber result = new RomanNumber(1500);
+
+            Assert.AreEqual(result.ToString(), (b + c).ToString());
+            Assert.ThrowsException<RomanNumberException>(() => a + d);
+            Assert.ThrowsException<RomanNumberException>(() => d + d);
+            Assert.ThrowsException<RomanNumberException>(() => n + c);
+            Assert.ThrowsException<RomanNumberException>(() => c + n);
+            Assert.ThrowsException<RomanNumberException>(() => n + n);
+        }
+        [TestMethod()]
+        public void SubTest()
+        {
+            RomanNumber a = new RomanNumber(1);
+            RomanNumber b = new RomanNumber(500);
+            RomanNumber c = new RomanNumber(1000);
+            RomanNumber d = new RomanNumber(3999);
+            RomanNumber n = null;
+
+            RomanNumber result = new RomanNumber(500);
+
+            Assert.AreEqual(result.ToString(), (c - b).ToString());
+            Assert.ThrowsException<RomanNumberException>(() => a - d);
+            Assert.ThrowsException<RomanNumberException>(() => c - c);
+            Assert.ThrowsException<RomanNumberException>(() => n - c);
+            Assert.ThrowsException<RomanNumberException>(() => c - n);
+            Assert.ThrowsException<RomanNumberException>(() => n - n);
+        }
+        [TestMethod()]
+        public void MulTest()
+        {
+            RomanNumber b = new RomanNumber(25);
+            RomanNumber c = new RomanNumber(30);
+            RomanNumber d = new RomanNumber(3999);
+            RomanNumber e = new RomanNumber(20);
+            RomanNumber f = new RomanNumber(200);
+            RomanNumber n = null;
+
+            RomanNumber result = new RomanNumber(750);
+
+            Assert.AreEqual(result.ToString(), (b * c).ToString());
+            Assert.ThrowsException<RomanNumberException>(() => d * d);
+            Assert.ThrowsException<RomanNumberException>(() => e * f);
+            Assert.ThrowsException<RomanNumberException>(() => n * c);
+            Assert.ThrowsException<RomanNumberException>(() => c * n);
+            Assert.ThrowsException<RomanNumberException>(() => n * n);
+        }
+        [TestMethod()]
+        public void DivTest()
+        {
+            RomanNumber a = new RomanNumber(1);
+            RomanNumber b = new RomanNumber(500);
+            RomanNumber c = new RomanNumber(1000);
+            RomanNumber d = new RomanNumber(3999);
+            RomanNumber n = null;
+
+            RomanNumber result = new RomanNumber(2);
+
+            Assert.AreEqual(result.ToString(), (c / b).ToString());
+            Assert.ThrowsException<RomanNumberException>(() => a / d);
+            Assert.ThrowsException<RomanNumberException>(() => n / c);
+            Assert.ThrowsException<RomanNumberException>(() => c / n);
+            Assert.ThrowsException<RomanNumberException>(() => n / n);
+        }
+        [TestMethod()]
+        public void CloneTest()
+        {
+            RomanNumber a = new RomanNumber(1234);
+            RomanNumber b = (RomanNumber)a.Clone();
+
+            Assert.AreEqual(a.ToString(), b.ToString());
+            Assert.AreNotSame(a, b);
+        }
+        [TestMethod()]
+        public void CompareTest()
+        {
+            RomanNumber a = new RomanNumber(1234);
+            RomanNumber b = new RomanNumber(3210);
+            RomanNumber c = null;
+
+            Assert.IsTrue(a.CompareTo(b) > 0);
+            Assert.IsTrue(b.CompareTo(a) < 0);
+            Assert.IsTrue(a.CompareTo(a) == 0);
+
+            Assert.ThrowsException<RomanNumberException>(() => b.CompareTo(c));
+        }
+    }
+}
